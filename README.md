1f9ba601229b2b76a686d40a3173941c54368beb
Bernardo Costa Nascimento <bernardoc1104@gmail.com>
2020-09-07 11:07:27 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# TDSA-MedBD
---
+++ TDSA-MedBDTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TDSA_MedBDTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
